0d4b7e4d86bf611be453ea860c32559a549bc551
abhi <abhiifour@gmail.com>
2026-01-02 15:50:53 +0530

    fix: icons

--- components/icon.tsx
+++ components/icon.tsx
@@ -5,31 +5,24 @@ import {AnimatePresence, motion}  from 'motion/react'
 export default function Icon(){
 
     const [alarmSet, setAlarmSet] = useState<boolean>(false)
+    const D_PATH1 = "m9 13 2 2 4-4"
+    const D_PATH2 = "M12 9v4l2 2"
     return (
         <div onClick={() => setAlarmSet(prev => !prev)}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-alarm-clock-icon lucide-alarm-clock">
             <circle cx="12" cy="13" r="8"/>
-           <AnimatePresence initial={false}>
-          {alarmSet ? (
-            <motion.path
-              key="check"
-              d="m9 13 2 2 4-4"
-              initial={{ pathLength: 0, opacity: 0, }}
-              animate={{ pathLength: 1, opacity: 1 }}
-              exit={{ pathLength: 0, opacity: 0, filter:"blur(1px)"}}
-              transition={{ duration: 0.2, ease: "easeOut" }}
-            />
-          ) : (
-            <motion.path
-              key="hands"
-              d="M12 9v4l2 2"
-              initial={{ pathLength: 0, opacity: 0 }}
-              animate={{ pathLength: 1, opacity: 1 }}
-              exit={{ pathLength: 0, opacity: 0,filter:"blur(1px)" }}
-              transition={{ duration: 0.2, ease: "easeOut" }}
-            />
-          )}
-        </AnimatePresence>
+          
+          
+           <motion.path
+                key={alarmSet ? "on" : "off"}
+                d={alarmSet ? D_PATH1 : D_PATH2}
+                
+                initial={{ pathLength: 0, opacity: 0 }}
+                animate={{ pathLength: 1, opacity: 1 }}
+                transition={{ duration: 0.2, ease: "easeOut" }}
+                />
+
+
 
             {
                 alarmSet ? <motion.path 
